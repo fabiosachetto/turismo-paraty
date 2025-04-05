e9db9963876ccca4d981d8f0b16d4bee3b60909e
verdi <fabio@sachetto.com.br>
2025-04-05 16:25:47 -0300

    primeiro commit

---
+++ turismo-paraty-app/src/app/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1 className="m-auto uppercase font-bold text-5xl">Turismo Paraty</h1>
+
+        <Image
+          className=""
+          src="/em_construcao.png"
+          alt="Turismo Paraty"
+          width={730}
+          height={453}
+          priority
+        />
+
+        <p className="m-auto uppercase font-bold">Site em Construção!!!</p>
+
+        <span className="m-auto">Em Desenvolvimento por Brain Tech.</span>
+      </main>
+    </div>
+  );
+}
