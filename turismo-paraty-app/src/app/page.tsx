3af1378e3db9e5cb5a9e8d4af81e757601ee5d44
verdi <fabio@sachetto.com.br>
2025-04-06 18:11:28 -0300

    centralizando h1 para mobile

--- turismo-paraty-app/src/app/page.tsx
+++ turismo-paraty-app/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="m-auto uppercase font-bold text-5xl">Turismo Paraty</h1>
+        <h1 className="m-auto uppercase font-bold text-5xl text-center">Turismo Paraty</h1>
 
         <Image
           className=""
